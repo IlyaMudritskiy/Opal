7df0e4478c9e8f0f78d1d003974f59edc99ef27b
Ilya Mudritskiy <ilya.mudritskiy@sonova.com>
2023-12-15 12:03:05 +0000

    Event-containers for main UI, events moved from MainForm.

---
+++ src/Controller/UI/SettingsController.cs
@@ -0,0 +1,31 @@
+﻿using ProcessDashboard.src.Model.AppConfiguration;
+using System;
+
+namespace ProcessDashboard.src.Controller.UI
+{
+    public class SettingsController
+    {
+        private Config config = Config.Instance;
+
+        private MainForm mainForm;
+
+        public SettingsController(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+        }
+
+        internal void OnOffAcousticPlotsMenuButton_Click(object sender, EventArgs e)
+        {
+            config.Acoustic.Enabled = !config.Acoustic.Enabled;
+            config.Save();
+            mainForm.OnOffAcousticPlotsMenuButton.Checked = config.Acoustic.Enabled;
+        }
+
+        internal void FileSelectionTypeMenuButton_Click(object sender, EventArgs e)
+        {
+            config.Acoustic.ManualSelection = !config.Acoustic.ManualSelection;
+            config.Save();
+            mainForm.FileSelectionTypeMenuButton.Checked = config.Acoustic.ManualSelection;
+        }
+    }
+}
---
+++ src/Controller/UI/UIController.cs
@@ -0,0 +1,22 @@
+﻿namespace ProcessDashboard.src.Controller.UI
+{
+    public class UIController
+    {
+        public FileController File { get; private set; }
+        public SettingsController Settings { get; private set; }
+
+        private MainForm mainForm;
+
+        public UIController(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            File = new FileController(mainForm);
+            Settings = new SettingsController(mainForm);
+        }
+    }
+}
