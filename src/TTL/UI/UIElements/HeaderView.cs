2c23e0026f5ad47ab37995c654b82116542b6e45
Ilya Mudritskiy <ilya.mudritskiy@sonova.com>
2024-02-08 11:06:36 +0000

    Created HeaderView

---
+++ src/TTL/UI/UIElements/HeaderView.cs
@@ -0,0 +1,68 @@
+﻿using ProcessDashboard.src.TTL.Screen;
+using ProcessDashboard.src.Utils;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProcessDashboard.src.TTL.UI.UIElements
+{
+    public abstract class HeaderView<T> where T : Control
+    {
+        public Label Title { get; private set; }
+        public TableLayoutPanel Layout { get; private set; }
+        public T Control { get; private set; }
+
+        public HeaderView()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Title = new Label()
+            {
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleCenter,
+                BackColor = Colors.Black,
+                ForeColor = Colors.White,
+                Font = Fonts.Sennheiser.ML,
+            };
+
+            Layout = new TableLayoutPanel()
+            {
+                ColumnCount = 1,
+                RowCount = 2,
+                Dock = DockStyle.Fill,
+                BackColor = Colors.Black,
+                ColumnStyles = { new ColumnStyle(SizeType.Percent, 100) },
+                RowStyles =
+                {
+                    new RowStyle(SizeType.Absolute, 40),
+                    new RowStyle(SizeType.Percent, 100)
+                }
+            };
+
+            Layout.SuspendLayout();
+            Layout.Controls.Add(Title, 0, 0);
+            Layout.ResumeLayout();
+        }
+
+        public void Color(Color color)
+        {
+            Layout.BackColor = color;
+            Title.BackColor = color;
+        }
+
+        public void AddControl(T control)
+        {
+            Control = control;
+            Layout.SuspendLayout();
+            Layout.Controls.Add(control, 0, 1);
+            Layout.ResumeLayout();
+        }
+
+        public void SetText(string title)
+        {
+            Title.Text = title;
+        }
+    }
+}
