d1c93e17ca42b2a883a004badf0c34d6baf6a2bb
Ilya Mudritskiy <ilya.mudritskiy@sonova.com>
2024-05-28 12:03:56 +0100

    ASx Acoustic Reports adaptation

--- src/TTL/Containers/ScreenData/TTLAcoustic.cs
+++ src/TTL/Containers/ScreenData/TTLAcoustic.cs
@@ -18,6 +18,7 @@ namespace ProcessDashboard.src.TTL.Containers.ScreenData
         public int TrackNumber { get; set; }
         public int PressNumber { get; set; }
         public List<FailReason> FailReasons { get; set; }
+        public List<StepStatus> StepsStatus { get; set; }
 
         public Measurements2DExt FR { get; set; }
         public Measurements2DExt THD { get; set; }
@@ -32,6 +33,7 @@ namespace ProcessDashboard.src.TTL.Containers.ScreenData
         public TTLAcoustic(AcousticFile file)
         {
             FailReasons = new List<FailReason>();
+            StepsStatus = new List<StepStatus>();
 
             Pass = file.DUT.Pass;
             Serial = file.DUT.Serial;
@@ -40,9 +42,9 @@ namespace ProcessDashboard.src.TTL.Containers.ScreenData
             TrackNumber = file.DUT.Track;
             PressNumber = file.DUT.Press;
 
-            FR = getMeasurement(file, "freq");
+            FR = getMeasurement(file, "fr");
             THD = getMeasurement(file, "thd");
-            RNB = getMeasurement(file, "rnb");
+            RNB = getMeasurement(file, "rbz");
             IMP = getMeasurement(file, "imp");
 
             FRCurve = new ScatterPlot(ToLogScale(FR.X), FR.Y.ToArray())
@@ -71,11 +73,40 @@ namespace ProcessDashboard.src.TTL.Containers.ScreenData
             };
 
             FindFailReasons();
+            AddStepsStatus(file);
         }
 
         private Measurements2DExt getMeasurement(AcousticFile file, string stepname)
         {
-            var content = file.Steps.Where(x => x.StepName == stepname).FirstOrDefault();
+            if (file == null) return null;
+
+            string step = StepNameGetter.GetStepName(stepname);
+            
+            if (step == "FR")
+            {
+                var acStep = getASxFR(file);
+                return new Measurements2DExt()
+                {
+                    Pass = acStep.StepPass,
+                    X = acStep.Measurement[0].ToList(),
+                    Y = acStep.Measurement[1].ToList()
+                };
+            }
+
+            if (step == "THD")
+            {
+                var acStep = getASxTHD(file);
+                return new Measurements2DExt()
+                {
+                    Pass = acStep.StepPass,
+                    X = acStep.Measurement[0].ToList(),
+                    Y = acStep.Measurement[1].ToList()
+                };
+            }
+
+            AcousticStep content = file.Steps.Where(x => x.StepName == step).FirstOrDefault();
+
+            if (content == null) return null;
 
             return new Measurements2DExt()
             {
@@ -85,8 +116,118 @@ namespace ProcessDashboard.src.TTL.Containers.ScreenData
             };
         }
 
+        private AcousticStep getASxFR(AcousticFile file)
+        {
+            List<double> freq = new List<double>();
+            List<double> dbspl = new List<double>();
+            AcousticStep result = new AcousticStep()
+            {
+                StepName = "FR",
+                StepPass = true,
+                Measurement = new List<double[]>()
+            };
+
+            foreach (var stepData in file.Steps)
+            {
+                if (stepData.StepName == "FR Low +")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    dbspl.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+
+                if (stepData.StepName == "FR Low -")
+                    result.StepPass = result.StepPass && stepData.StepPass;
+
+                if (stepData.StepName == "FR Low Mid +")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    dbspl.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+
+                if (stepData.StepName == "FR Low Mid -")
+                    result.StepPass = result.StepPass && stepData.StepPass;
+
+                if (stepData.StepName == "FR High Mid +")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    dbspl.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+
+                if (stepData.StepName == "FR High Mid -")
+                    result.StepPass = result.StepPass && stepData.StepPass;
+
+                if (stepData.StepName == "FR High +")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    dbspl.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+
+                if (stepData.StepName == "FR High -")
+                    result.StepPass = result.StepPass && stepData.StepPass;
+            }
+            result.Measurement.Add(freq.ToArray());
+            result.Measurement.Add(dbspl.ToArray());
+            return result;
+        }
+
+        private AcousticStep getASxTHD(AcousticFile file)
+        {
+            List<double> freq = new List<double>();
+            List<double> thd = new List<double>();
+
+            AcousticStep result = new AcousticStep()
+            {
+                StepName = "THD",
+                StepPass = true,
+                Measurement = new List<double[]>()
+            };
+
+            foreach (var stepData in file.Steps)
+            {
+                if (stepData.StepName == "THD Low")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    thd.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+                if (stepData.StepName == "THD Mid")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    thd.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+                if (stepData.StepName == "THD High")
+                {
+                    freq.AddRange(stepData.Measurement[0]);
+                    thd.AddRange(stepData.Measurement[1]);
+                    result.StepPass = result.StepPass && stepData.StepPass;
+                }
+            }
+            result.Measurement.Add(freq.ToArray());
+            result.Measurement.Add(thd.ToArray());
+
+            return result;
+        }
+
+        private void AddStepsStatus(AcousticFile file)
+        {
+            foreach (var step in file.Steps)
+            {
+                StepsStatus.Add(new StepStatus
+                {
+                    StepName = step.StepName,
+                    StepPass = step.StepPass,
+                });
+            }
+        }
+
         private void FindFailReasons()
         {
+            
             if (Pass) return;
 
             if (!FR.Pass) FailReasons.Add(FailReason.FR);
