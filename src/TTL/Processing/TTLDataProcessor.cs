0959fa226954c40dba0e3be42905bee7e79f7957
Ilya Mudritskiy <ilya.mudritskiy@sonova.com>
2024-07-15 13:09:42 +0100

    No lookup and loading of acoustic data when Acoustic is disabled

--- src/TTL/Processing/TTLDataProcessor.cs
+++ src/TTL/Processing/TTLDataProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using ProcessDashboard.Model.AppConfiguration;
 using ProcessDashboard.Model.Data.Acoustic;
 using ProcessDashboard.src.TTL.Containers.FileContent;
 using ProcessDashboard.src.TTL.Containers.ScreenData;
@@ -12,6 +13,7 @@ namespace ProcessDashboard.src.TTL.Processing
     public static class TTLDataProcessor
     {
         private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+        private static Config config = Config.Instance;
 
         public async static Task<List<TTLUnit>> LoadFiles(List<JObject> files)
         {
@@ -31,15 +33,18 @@ namespace ProcessDashboard.src.TTL.Processing
         private static List<TTLUnit> JoinFiles(IEnumerable<ProcessFile> processFiles, IEnumerable<AcousticFile> acousticFiles)
         {
             ConcurrentBag<TTLUnit> result = new ConcurrentBag<TTLUnit>();
-            ConcurrentBag<ProcessFile> pf = new ConcurrentBag<ProcessFile>(processFiles);
-            ConcurrentBag<AcousticFile> af = new ConcurrentBag<AcousticFile>(acousticFiles);
+            //ConcurrentBag<ProcessFile> pf = new ConcurrentBag<ProcessFile>(processFiles);
+            //ConcurrentBag<AcousticFile> af = new ConcurrentBag<AcousticFile>(acousticFiles);
 
             Parallel.ForEach(processFiles, file =>
             {
                 string serial = file.DUT.SerialNumber;
-                AcousticFile acousticFile = acousticFiles.Where(s => s.DUT.Serial == serial).FirstOrDefault();
-
-                result.Add(new TTLUnit(file, acousticFile));
+                if (acousticFiles != null)
+                {
+                    AcousticFile acousticFile = acousticFiles.Where(s => s.DUT.Serial == serial).FirstOrDefault();
+                    result.Add(new TTLUnit(file, acousticFile));
+                }
+                result.Add(new TTLUnit(file, null));
             });
 
             return result.ToList();
