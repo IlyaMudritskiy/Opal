54ecc34a6d4bf1dd1ba6499d4bae974d98edee86
Ilya Mudritskiy <ilya.mudritskiy@sonova.com>
2024-08-13 13:12:34 +0100

    Initial UI changes

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Opal.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProcessDashboard.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Opal.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
